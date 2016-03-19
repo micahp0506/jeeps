5305f8fa4cbe8f699ad4fa46d90478955a32e84b
Micah Wells <micahp0506@gmail.com>
2016-03-19 14:25:15 -0500

    Created navbar

---
+++ client/js/components/Main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+
+import React from 'react';
+import Navbar from './Navbar';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Navbar history={this.props.history} />
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ client/js/components/Navbar.js
@@ -0,0 +1,17 @@
+'use strict';
+
+
+import React from 'react';
+import UserActions from '../actions/UserActions';
+
+export default React.createClass({
+  render() {
+    return (
+        <div className="ui inverted menu navbar ">
+            <a href="#" className="brand item logo-container">Jeepers</a>
+            <a className="item" href="#/home">Home</a>
+            <a className="item" id="login" href="#/login">Log In</a>
+        </div>
+    )
+  }
+})
