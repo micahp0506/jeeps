75db301e1dbe4e4b5e69a203ca7a2d6f4fb4611e
Micah Wells <micahp0506@gmail.com>
2016-03-21 08:25:39 -0500

    Correcting build errors

--- client/js/main.js
+++ client/js/main.js
@@ -5,8 +5,8 @@ import Router from 'react-router';
 import ReactDOM from 'react-dom';
 import routes from './config/routes';
 import createBrowserHistory from 'history/lib/createBrowserHistory';
-import $ from '../bower_components/jquery/dist/jquery.min.js';
-import '../bower_components/semantic/dist/semantic.min.js';
+// import $ from './bower_components/jquery/dist/jquery.min.js';
+// import './bower_components/semantic/dist/semantic.min.js';
 let history = createBrowserHistory();
 
 
--- server/routes.js
+++ server/routes.js
@@ -3,7 +3,7 @@
 
 const express = require('express');
 const router = express.Router();
-const userController = require('./controllers/userController');
+const userController = require('./controllers/userControllers');
 
 
 router.post('/api/user/create', userController.registerUser);
