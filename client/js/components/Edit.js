282b3c095c40748a161bf1108a48cbcf4685826b
Micah Wells <micahp0506@gmail.com>
2016-04-09 16:29:06 -0500

    Setting up boiler plate for Eidt module

--- client/js/components/Edit.js
+++ client/js/components/Edit.js
@@ -1,1 +1,15 @@
 'use strict';
+
+
+import alt from '../utils/alt';
+import React from 'react';
+import EditActions from '../actions/EditActions';
+import EditStore from '../stores/EditStore';
+
+
+
+// Creating Search to handle actions and store
+class Edit extends React.Component {
+}
+
+export default Edit;
